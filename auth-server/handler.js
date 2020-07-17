dda2697863c5d82d9b8b5f561fa8277149c51fe6
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-07-17 19:11:03 +0200

    addded refresh token button to test.html

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -16,7 +16,7 @@ module.exports.getAccessToken = async (event) => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*', 'http://127.0.0.1:8080/test-auth-server.html'
     },
     body: JSON.stringify({
       access_token: info.data.access_token,
@@ -24,3 +24,29 @@ module.exports.getAccessToken = async (event) => {
     }),
   };
 };
+
+module.exports.getRefreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=unmfj4cn58bom36ulhs1r8ea8c'
+    + '&client_secret=uaqmj9ifo3cr4d174tniku1pvj'
+    + '&grant_type=refresh_token'
+    + '&redirect_uri=https://judithrohrer.github.io/meetup/'
+    + '&refresh_token=' + event.pathParameters.refresh_token;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', 'http://127.0.0.1:8080/test-auth-server.html'
+    },
+    body: JSON.stringify({
+      access_token: info.data.access_token,
+      refresh_token: info.data.refresh_token,
+    }),
+  };
+};
+
+// User authorization_code
+// https://secure.meetup.com/oauth2/authorize?client_id=unmfj4cn58bom36ulhs1r8ea8c&response_type=code&redirect_uri=https://judithrohrer.github.io/meetup/
